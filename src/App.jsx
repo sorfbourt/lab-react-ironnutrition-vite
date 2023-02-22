21a00591a59638a50860fb786dc249052c3ceefe
Krystina Witt <krystinawitt@gmail.com>
2023-02-22 14:47:34 +0100

    iteration 5 done

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,11 @@ import foods from './foods.json'
 import './App.css'
 import FoodBox from './components/FoodBox'
 import AddFoodForm from './components/AddFoodForm'
+import SearchBar from './components/SearchBar'
 
 function App() {
   const [foodsState, setFoodsState] = useState(foods)
+  const [searchString, setSearchString] = useState('')
   return (
     <div className="App">
       {/* Display Add Food component here */}
@@ -16,13 +18,20 @@ function App() {
 
       {/* Display Search component here */}
       <AddFoodForm setFoodsState={setFoodsState} />
-
+      <SearchBar setSearchString={setSearchString} searchString={searchString} setFoodsState={setFoodsState} foodsState={foodsState}/>
       <Divider>Food List</Divider>
 
       <Row style={{ width: '100%', justifyContent: 'center' }}>
         {/* Render the list of Food Box components here */}
-        {foodsState.map((food)=>{
-          return <FoodBox food={food}/>
+        {foodsState.filter((food)=>{
+          if(searchString ===""){
+            return food
+          }
+          if(food.name.toLowerCase().includes(searchString.toLowerCase())){
+            return food
+          }
+        }).map((food, index)=>{
+          return <FoodBox key={index} food={food}/>
         })}
       </Row>
     </div>
---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import { useState } from 'react'
+import { Card, Row, Col, Divider, Input, Button } from 'antd'
+
+function SearchBar({setSearchString, searchString, setFoodsState, foodsState}) {
+  /*   const [searchString, setSearchString] = useState("") */
+
+/*     const handleSearch = (event)=>{
+        event.preventDefault()
+        const foodFilters = [...foodsState]
+        setFoodsState(foodFilters.filter(food =>{
+            return food.name === searchString
+        }))
+
+    } */
+
+  return (
+    <div>
+        <form style={{ display: 'flex', flexDirection: 'column' }} /* onSubmit={handleSearch} */>
+
+        <label>Search: <Input value={searchString} type="text" onChange={event =>setSearchString(event.target.value)} /></label>
+
+        <button type="submit">Search</button>
+
+        </form>
+
+
+    </div>
+  )
+}
+
+export default SearchBar
